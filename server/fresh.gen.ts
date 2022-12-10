5590c741b2880a39eba3fc0efe8cce3f94084e2f
Travis Chapman <traviswchapman@gmail.com>
2022-12-10 15:32:07 -0500

    WIP discord login

--- server/fresh.gen.ts
+++ server/fresh.gen.ts
@@ -8,7 +8,8 @@ import * as $1 from "./routes/api/listings.ts";
 import * as $2 from "./routes/api/listings/[id].tsx";
 import * as $3 from "./routes/api/verify.ts";
 import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/verification.tsx";
+import * as $5 from "./routes/signin.tsx";
+import * as $6 from "./routes/verification.tsx";
 import * as $$0 from "./islands/Searchbar.tsx";
 
 const manifest = {
@@ -18,7 +19,8 @@ const manifest = {
     "./routes/api/listings/[id].tsx": $2,
     "./routes/api/verify.ts": $3,
     "./routes/index.tsx": $4,
-    "./routes/verification.tsx": $5,
+    "./routes/signin.tsx": $5,
+    "./routes/verification.tsx": $6,
   },
   islands: {
     "./islands/Searchbar.tsx": $$0,
---
+++ server/routes/signin.tsx
@@ -0,0 +1,54 @@
+// import { OAuth2Client } from "https://deno.land/x/oauth2_client/mod.ts";
+
+// const oauth2Client = new OAuth2Client({
+//     clientId: Deno.env.get("CLIENT_ID")!,
+//     clientSecret: Deno.env.get("CLIENT_SECRET")!,
+//     authorizationEndpointUri: "hhttps://discord.com/oauth2/authorize",
+//     tokenUri: "https://discord.com/api/oauth2/token",
+//     redirectUri: "http://localhost:8000/oauth2/callback",
+//     defaults: {
+//         scope: "identify",
+//     },
+// });
+
+// /** This is where we'll store our state and PKCE codeVerifiers */
+// const loginStates = new Map<string, { state: string; codeVerifier: string }>();
+// /** The name we'll use for the session cookie */
+// const cookieName = "session";
+
+// export const handler: Handlers = {
+//     async GET(req, ctx) {
+//         const url = new URL(req.url);
+//         const handle = url.searchParams.get("handle") || "";
+//         const textResponse = await fetch(`https://robertsspaceindustries.com/citizens/${handle}`);
+//         const textData = await textResponse.text();
+//         const document = new DOMParser().parseFromString(textData, "text/html");
+//         let bio = null;
+//         if (document) {
+//             bio = document.querySelector(".entry.bio")?.querySelector(".value");
+//         }
+//         const isVerified = bio?.textContent.includes(verficationCode);
+//         return ctx.render({ isVerified });
+//     },
+// };
+
+import DenoGrant, {
+    Providers,
+} from "https://deno.land/x/deno_grant@v0.3.0/mod.ts";
+
+const denoGrant = new DenoGrant({
+    base_uri: "http://localhost:8000",
+    strategies: [{
+        provider: Providers.discord,
+        client_id: Deno.env.get("CLIENT_ID")!,
+        client_secret: Deno.env.get("CLIENT_SECRET")!,
+        redirect_path: "/auth/discord/callback",
+        scope: "identify",
+    }]
+});
+
+const discordAuthorizationURI = denoGrant.getAuthorizationUri(Providers.discord)!.toString();
+
+export function handler(req: Request): Response {
+    return Response.redirect(discordAuthorizationURI);
+}
